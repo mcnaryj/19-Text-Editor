69340fbe4a5f30ebe4a3ef74a321629d7bd87724
Jack McNary <jack.mcnary@gmail.com>
2022-01-25 21:00:15 -0700

    building out registerRoute

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,6 +27,15 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
-// new StaleWhileRevalidate({
-//   new CacheableResponsePlugin({
+registerRoute(
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
+    cacheName: 'asset-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ]
+  })
+);
+// 
