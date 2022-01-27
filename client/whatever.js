bd23ed8c70b015b16304167925b7d123148c6064
Jack McNary <jack.mcnary@gmail.com>
2022-01-26 21:03:01 -0700

    by jove, it works

--- client/whatever.js
+++ client/whatever.js
@@ -1,11 +1,12 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
+// Require the GenerateSW class of the WorkBoxPlugin 
 const WorkboxPlugin = require('workbox-webpack-plugin');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+// TODO: Add and configure workbox plugins for a service worker and manifest file. DONE?
+// TODO: Add CSS loaders and babel to webpack. DONE?
 
 module.exports = () => {
   return {
@@ -21,42 +22,48 @@ module.exports = () => {
     plugins: [
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'JATE, sigh...',
+        title: 'i HATE JATE',
       }),
+      // new WorkboxPlugin.GenerateSW(),     
+      // Injects our custom service worker
       new InjectManifest({
         swSrc: './src-sw.js',
         swDest: 'src-sw.js',
       }),
-      // Creating the manifest.json
-      new WebpackPwaManifest({
-        fingerprints: false,
-        inject: true,
-        name: 'JATE Text Editor Logo',
-        short_name: 'JATE',
-        description: 'Something something JATE something',
-        background_color: '#225ca3',
-        theme_color: '#225ca3',
-        start_url: '/',
-        publicPath: '/',
-        icons: [
-          {
-            src: path.resolve('src/images/logo.png'),
-            sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join('assets', 'icons'),
-          },
-        ],
-      }),
+
+    // Creates a manifest.json file.
+    new WebpackPwaManifest({
+      fingerprints: false,
+      inject: true,
+      name: 'JATE Logo',
+      short_name: 'HATE',
+      description: 'Laurel strongly dislikes JATE!',
+      background_color: '#225ca3',
+      theme_color: '#225ca3',
+      start_url: '/',
+      publicPath: '/',
+      icons: [
+        {
+          src: path.resolve('src/images/logo.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('assets', 'icons'),
+        },
+      ],
+    }),
+// do i need this line??
+      // new WorkboxPlugin.GenerateSW()
     ],
 
     module: {
       rules: [
         {
           test: /\.css$/i,
-          use: ['style-loader', 'css-loader'],
+          use: ["style-loader", 'css-loader'],
         },
         {
           test: /\.m?js$/,
           exclude: /node_modules/,
+                    // We use babel-loader in order to use ES6.
           use: {
             loader: 'babel-loader',
             options: {
